525381e2a65bee12193bddda4a1bb4d26f6392b0
ekonof23 <ekonof28@gmail.com>
2024-02-04 00:02:53 +0700

    menambahkan env

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const port = 4000;
-const indexRouter = require("./controllers/index");
-const loggerMiddleware = require("./middlewares/loggerMiddleware");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3306;
+const indexRouter = require('./controllers/index');
+const loggerMiddleware = require('./middlewares/loggerMiddleware');
+const dotenv = require('dotenv').config();
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(dotenv);
 app.use(bodyParser.json());
 app.use(loggerMiddleware);
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (err, req, res, next) {
@@ -27,11 +29,11 @@ app.use(function (err, req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 app.listen(port, () => {
