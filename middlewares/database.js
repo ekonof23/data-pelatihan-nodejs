98c050d5c1dab9fc96b41719de2a60d1e4458ecf
ekonof23 <ekonof28@gmail.com>
2024-02-06 11:54:58 +0700

    konfigurasi db

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,10 +1,12 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
 var conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "data-pelatihan",
+  host: process.env.MYSQL_ADDON_HOST,
+  database: process.env.MYSQL_ADDON_DB,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  port: process.env.MYSQL_ADDON_PORT,
 });
 conn.connect(function (err) {
   if (err) throw err;
